31e8ee033c8edd1e892b3f16894e02b631af853a
MackyNous <maricio.jongma@hva.nl>
2018-05-04 10:09:29 +0200

    added gurkan file

---
+++ NDVI/gurkan.py
@@ -0,0 +1,98 @@
+# MET HULP VAN VIDJAI 
+
+import rasterio
+import numpy as np
+
+filename = "20180312_134231_0f4c_3B_AnalyticMS.tif"
+
+# Load red and NIR bands - note all PlanetScope 4-band images have band order BGRN
+with rasterio.open(filename) as src:
+    band_red = src.read(3)
+
+with rasterio.open(filename) as src:
+    band_nir = src.read(4)
+    
+from xml.dom import minidom
+
+xmldoc = minidom.parse("20180312_134231_0f4c_3B_AnalyticMS_metadata.xml")
+nodes = xmldoc.getElementsByTagName("ps:bandSpecificMetadata")
+
+# XML parser refers to bands by numbers 1-4
+coeffs = {}
+for node in nodes:
+    bn = node.getElementsByTagName("ps:bandNumber")[0].firstChild.data
+    if bn in ['1', '2', '3', '4']:
+        i = int(bn)
+        value = node.getElementsByTagName("ps:reflectanceCoefficient")[0].firstChild.data
+        coeffs[i] = float(value)
+        
+# Multiply the Digital Number (DN) values in each band by the TOA reflectance coefficients
+band_red = band_red * coeffs[3]
+band_nir = band_nir * coeffs[4]
+
+# Allow division by zero
+np.seterr(divide='ignore', invalid='ignore')
+
+# Calculate NDVI. This is the equation at the top of this guide expressed in code
+ndvi = (band_nir.astype(float) - band_red.astype(float)) / (band_nir + band_red)
+
+# check range NDVI values, excluding NaN
+np.nanmin(ndvi), np.nanmax(ndvi)
+
+# Set spatial characteristics of the output object to mirror the input
+kwargs = src.meta
+kwargs.update(
+    dtype=rasterio.float32,
+    count = 1)
+
+# Write band calculations to a new raster file
+with rasterio.open('ndvi.tif', 'w', **kwargs) as dst:
+        dst.write_band(1, ndvi.astype(rasterio.float32))
+        
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
+"""
+The NDVI values will range from -1 to 1. You want to use a diverging color scheme to visualize the data,
+and you want to center the colorbar at a defined midpoint. The class below allows you to normalize the colorbar.
+"""
+
+class MidpointNormalize(colors.Normalize):
+    """
+    Normalise the colorbar so that diverging bars work there way either side from a prescribed midpoint value)
+    e.g. im=ax1.imshow(array, norm=MidpointNormalize(midpoint=0.,vmin=-100, vmax=100))
+    Credit: Joe Kington, http://chris35wills.github.io/matplotlib_diverging_colorbar/
+    """
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
+
+
+# Set min/max values from NDVI range for image (excluding NAN)
+# set midpoint according to how NDVI is interpreted: https://earthobservatory.nasa.gov/Features/MeasuringVegetation/
+min=np.nanmin(ndvi)
+max=np.nanmax(ndvi)
+mid=0.1
+
+fig = plt.figure(figsize=(20,10))
+ax = fig.add_subplot(111)
+
+# diverging color scheme chosen from https://matplotlib.org/users/colormaps.html
+cmap = plt.cm.RdYlGn
+
+cax = ax.imshow(ndvi, cmap=cmap, clim=(min, max), norm=MidpointNormalize(midpoint=mid,vmin=min, vmax=max))
+
+ax.axis('off')
+ax.set_title('Normalized Difference Vegetation Index of Granada, Spain', fontsize=18, fontweight='bold')
+
+cbar = fig.colorbar(cax, orientation='horizontal', shrink=0.65)
+
+fig.savefig("ndvi-fig.png", dpi=200, bbox_inches='tight', pad_inches=0.7)
+plt.show()
+
